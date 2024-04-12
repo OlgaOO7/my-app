b3015d087a6c4ef7c6623adc9cc1770d5793a458
Olga007 <o.piankova@ukr.net>
2024-04-12 18:18:14 +0300

    Change styles and add MaterialUI components for Movie and MovieCard

--- src/features/Movies/Movies.tsx
+++ src/features/Movies/Movies.tsx
@@ -4,10 +4,10 @@ import { connect } from "react-redux";
 import { Movie, fetchMovies } from "../../reducers/movies";
 import { RootState } from "../../store";
 import { MovieCard } from "./MovieCard";
-
-
-import styles from './Movies.module.scss';
 import { useAppDispatch } from "../../hooks";
+
+import { Container } from "@mui/system";
+import { Grid, LinearProgress, Typography } from "@mui/material";
 
 interface MoviesProps {
   movies: Movie[];
@@ -23,23 +23,26 @@ function Movies({ movies, loading }: MoviesProps) {
   }, [dispatch])
 
   return (
-    <section>
-      <div className={styles.list}>
+    <Container sx={{ py: 8 }} maxWidth="lg">
+      <Typography variant="h4" align="center" gutterBottom>Now playing</Typography>
         {loading ? (
-          <h3>Loading...</h3>
-        ) : (
-          movies.map((m) => (
-          <MovieCard
+          <LinearProgress color="secondary"  />
+      ) : (
+          <Grid container spacing={4}>
+            {movies.map((m) => (
+            <Grid item key={m.id} xs={12} sm={6} md={4} >
+                <MovieCard
             id={m.id}
             title={m.title}
             overview={m.overview}
             popularity={m.popularity}
             image={m.image}
           />
-        ))
+            </Grid>
+        ))}
+        </Grid>
       )}
-    </div>
-  </section>
+  </Container>
   );
 }
 
